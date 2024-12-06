79aca838bfaebfb4df2de424e97ee92c100f3651
intikhab-h-bhat <intikhabh@gmail.com>
2024-12-06 16:16:05 +0530

    overloaded methods

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Random dice= new Random();
-int number= dice.Next(1,7);
-Console.WriteLine(number);
+// Random dice= new Random();
+
+// int roll=dice.Next();
+// int roll1= dice.Next(1,7);
+// int roll2= dice.Next(9);
+
+// Console.WriteLine($"First Roll: {roll}");
+// Console.WriteLine($"Second Roll: {roll1}");
+
+// Console.WriteLine($"Third Roll: {roll2}");
+
+
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue=Math.Max(firstValue,secondValue);
+
+Console.WriteLine(largerValue);
+
