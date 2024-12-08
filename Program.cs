00c684e5b4f714efcf4d4c50fe67465f24cbaa32
intikhab-h-bhat <intikhabh@gmail.com>
2024-12-08 18:23:30 +0530

    dice game

--- Program.cs
+++ Program.cs
@@ -11,10 +11,26 @@
 // Console.WriteLine($"Third Roll: {roll2}");
 
 
+// int firstValue = 500;
+// int secondValue = 600;
+// int largerValue=Math.Max(firstValue,secondValue);
 
-int firstValue = 500;
-int secondValue = 600;
-int largerValue=Math.Max(firstValue,secondValue);
+// Console.WriteLine(largerValue);
 
-Console.WriteLine(largerValue);
+Random dice= new Random();
+int roll1= dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+int roll3 = dice.Next(1,7);
+
+int total= roll1 + roll2 + roll3;
+Console.WriteLine($"Dice Roll :{roll1} + {roll2} + {roll3} = {total}");
+
+if(total>14){
+    Console.WriteLine("You win");
+}
+else if(total<15)
+{
+    Console.WriteLine("You Loose");
+}
+
 
