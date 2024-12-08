8c2cb8c36f0c3ae80d21173fb99b2e50bc85353e
intikhab-h-bhat <intikhabh@gmail.com>
2024-12-08 18:38:48 +0530

    dice game with if else statement

--- Program.cs
+++ Program.cs
@@ -25,10 +25,23 @@ int roll3 = dice.Next(1,7);
 int total= roll1 + roll2 + roll3;
 Console.WriteLine($"Dice Roll :{roll1} + {roll2} + {roll3} = {total}");
 
-if(total>14){
+if((roll1==roll2)||(roll2==roll3)||(roll1==roll3))
+{
+    if((roll1==roll2)&&(roll1==roll3))
+    {
+    Console.WriteLine("Yo get +6 bonus");
+    total+=6;
+    }
+    else
+    {
+    Console.WriteLine("You have bonus +2");
+    total+=2;
+    }
+}
+if(total>=15){
     Console.WriteLine("You win");
 }
-else if(total<15)
+else 
 {
     Console.WriteLine("You Loose");
 }
