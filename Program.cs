89af8c70693cbf908b36bf8bd1e6842fb2dac91e
intikhab-h-bhat <intikhabh@gmail.com>
2024-12-10 18:17:18 +0530

    white space

--- Program.cs
+++ Program.cs
@@ -94,10 +94,55 @@ using System.ComponentModel.DataAnnotations;
 
 //*************** code chalange***************
 
-string[] fradulentIds=["B123","c234","A345","C15","B177","G3003","C235","B179"];
+// string[] fradulentIds=["B123","c234","A345","C15","B177","G3003","C235","B179"];
 
-foreach(string fid in fradulentIds){
-    if(fid.StartsWith("B")){
-    Console.WriteLine(fid);
-    }
-}
+// foreach(string fid in fradulentIds){
+//     if(fid.StartsWith("B")){
+//     Console.WriteLine(fid);
+//     }
+// }
+
+//***************Code comments****
+// Random random = new Random();
+// string[] orderIDs = new string[5];
+// // Loop through each blank orderID
+// for (int i = 0; i < orderIDs.Length; i++)
+// {
+//     // Get a random value that equates to ASCII letters A through E
+//     int prefixValue = random.Next(65, 70);
+//     // Convert the random value into a char, then a string
+//     string prefix = Convert.ToChar(prefixValue).ToString();
+//     // Create a random number, pad with zeroes
+//     string suffix = random.Next(1, 1000).ToString("000");
+//     // Combine the prefix and suffix together, then assign to current OrderID
+//     orderIDs[i] = prefix + suffix;
+// }
+// // Print out each orderID
+// foreach (var orderID in orderIDs)
+// {
+//     Console.WriteLine(orderID);
+// }
+/*
+converts the string to array and 
+count the ocuurance of character o and then 
+convert back  to string and then prints the output
+*/
+string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+
+int x = 0;
+// count the o's
+foreach (char i in charMessage)
+ { 
+    if (i == 'o')
+     {
+         x++;
+     } 
+ }
+// convert it back to a string
+string new_message = new String(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
